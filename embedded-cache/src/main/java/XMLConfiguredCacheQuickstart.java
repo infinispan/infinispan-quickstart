7f5e4c077515c88d18534485f79ae179f887c49a
Pete Muir <pmuir@bleepbleep.org.uk>
2011-08-02 13:33:41 +0100

    minor

--- embedded-cache/src/main/java/XMLConfiguredCacheQuickstart.java
+++
@@ -1,8 +0,0 @@
-import org.infinispan.*;
-import org.infinispan.manager.*;
-
-public class XMLConfiguredCacheQuickstart {
-   public static void main(String args[]) throws Exception {
-	   Cache<?, ?> c = new DefaultCacheManager("infinispan.xml").getCache("xml-configured-cache");
-   }
-}
