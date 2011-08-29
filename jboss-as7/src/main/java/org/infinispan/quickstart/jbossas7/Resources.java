ddf977adb5eaaf0845314081c6a98905f8830806
Kevin Pollet <pollet.kevin@gmail.com>
2011-08-29 21:01:43 +0200

    Use Infinispan CDI support for Cache injection.

--- jboss-as7/src/main/java/org/infinispan/quickstart/jbossas7/Resources.java
+++ jboss-as7/src/main/java/org/infinispan/quickstart/jbossas7/Resources.java
@@ -1,35 +1,26 @@
 package org.infinispan.quickstart.jbossas7;
 
+import org.infinispan.cdi.OverrideDefault;
+import org.infinispan.manager.EmbeddedCacheManager;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.New;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 
-import org.infinispan.Cache;
-import org.infinispan.manager.CacheContainer;
-
-@Singleton
-@Startup
 public class Resources {
 
+   @Produces
+   @OverrideDefault
    @Resource(lookup="java:jboss/infinispan/jboss-as7-quickstart")
-   private CacheContainer container;
+   private EmbeddedCacheManager container;
 
    @Inject @New
    private LoggingListener loggingListener;
    
    @PostConstruct
-   public void startup() {
-      getCache().addListener(loggingListener);
+   private void startup() {
+      container.getCache().addListener(loggingListener);
    }
-   
-   @Produces @ApplicationScoped
-   public Cache<Object, Object> getCache() {
-      return this.container.getCache();
-   }
-
 }
